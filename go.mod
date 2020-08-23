80ba3656aa25c6745db6162a547d0d89de83b90c
Artur Khantimirov <yokree@gmail.com>
2020-08-23 14:45:33 +1200

    v1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/r2k1/gomod-versioning-2a
+
+go 1.14
+
+require github.com/r2k1/gomod-versioning-1a v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,7 @@
+package gomod_versioning_2a
+
+import gomodversioning1a "github.com/r2k1/gomod-versioning-1a"
+
+func Func2() error {
+	return gomodversioning1a.Func()
+}
